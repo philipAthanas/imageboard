92cc08bcbad6c440be823699924a4f2c9985ce72
philipAthanas <athanas@gmx.de>
2022-11-23 14:07:52 +0100

    modal pops up opens and closes

--- public/components/modal.js
+++ public/components/modal.js
@@ -19,30 +19,15 @@ const Modal = {
     },
     props: ["image"],
     mounted() {
-        // console.log("image in mounted", this.image.id);
-        this.image.id = this.id;
-        console.log("Here is my IMAGE ID: ", this.image.id);
-
-        fetch(`/images/${this.image.id}`).then((res) =>
-            res
-                .json()
-                .then((images) => {
-                    this.image.url = images.url;
-                    this.image.title = images.title;
-                    this.image.description = images.description;
-                    this.image.username = images.username;
-                })
-                .catch((err) => console.log("No image in modal"), error)
-        );
+        console.log("image in mounted", this.image);
     },
-
     template: `<div class="modal">
                 <button type="button" class="close" 
     @click="close"> X 
 </button>
     <div class="modalImgContainer">
-    <h1> {{ image.title }} </h1>
-    <img style="width:90%;" :src="images.url"  />
+    <h1> {{ image.title}} </h1>
+    <img style="width:90%;" :src="image.url"  />
     <p>{{ image.description }}</p> 
         <p>uploaded by {{ image.username }}</p>         
     </div>
